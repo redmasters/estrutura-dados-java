b98744afe27532ce391a69dedd606f44ec6ee002
Erasmo Jr <erasmo.junior@gmail.com>
2023-01-28 23:20:01 -0300

    :sparkles: AULA03: Metodo adiciona elemento

--- src/main/java/io/red/vetor/Vetor.java
+++ src/main/java/io/red/vetor/Vetor.java
@@ -8,5 +8,14 @@ public class Vetor {
         this.elementos = new String[capacidade];
     }
 
+    public void adiciona(String elemento){
+        for (int i = 0; i < this.elementos.length; i++){
+            if(this.elementos[i] == null){
+                this.elementos[i] = elemento;
+                break;
+            }
+        }
+
+    }
 
 }
--- src/main/java/io/red/vetor/test/Aula03.java
+++ src/main/java/io/red/vetor/test/Aula03.java
@@ -1,2 +1,13 @@
-package io.red.vetor.test;public class Aula03 {
+package io.red.vetor.test;
+
+import io.red.vetor.Vetor;
+
+public class Aula03 {
+    public static void main(String[] args) {
+        Vetor vetor = new Vetor(5);
+        vetor.adiciona("elemento 1");
+        vetor.adiciona("elemento 2");
+
+    }
+
 }
