fb49036187181d8c8d483587879b69af638e94c0
Erasmo Jr <erasmo.junior@gmail.com>
2023-01-28 23:43:25 -0300

    :sparkles: AULA03: Metodo adiciona elemento melhorado (com error)

--- src/main/java/io/red/vetor/Vetor.java
+++ src/main/java/io/red/vetor/Vetor.java
@@ -3,19 +3,32 @@ package io.red.vetor;
 public class Vetor {
 
     private String[] elementos;
+    private int tamanho;
 
-    public Vetor(int capacidade){
+    public Vetor(int capacidade) {
         this.elementos = new String[capacidade];
+        this.tamanho = 0;
     }
 
-    public void adiciona(String elemento){
-        for (int i = 0; i < this.elementos.length; i++){
-            if(this.elementos[i] == null){
-                this.elementos[i] = elemento;
-                break;
-            }
+//    public void adiciona(String elemento){
+//        for (int i = 0; i < this.elementos.length; i++){
+//            if(this.elementos[i] == null){
+//                this.elementos[i] = elemento;
+//                break;
+//            }
+////        }
+//    }
+
+        public void adiciona(String elemento) throws Exception {
+
+        if (this.tamanho < this.elementos.length) {
+            this.elementos[this.tamanho] = elemento;
+            this.tamanho++;
         }
+        throw new Exception("Vetor cheio");
 
     }
+
+
 
 }
--- src/main/java/io/red/vetor/test/Aula03.java
+++ src/main/java/io/red/vetor/test/Aula03.java
@@ -4,9 +4,16 @@ import io.red.vetor.Vetor;
 
 public class Aula03 {
     public static void main(String[] args) {
-        Vetor vetor = new Vetor(5);
-        vetor.adiciona("elemento 1");
-        vetor.adiciona("elemento 2");
+        Vetor vetor = new Vetor(2);
+        try {
+            vetor.adiciona("elemento 1");
+            vetor.adiciona("elemento 2");
+            vetor.adiciona("elemento 3");
+        } catch (Exception e) {
+            throw new RuntimeException(e);
+        }
+
+
 
     }
 
